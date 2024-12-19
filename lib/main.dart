a9c34c76561ed9ea1140583c3c955653e8ef95e8
joceesagar <joshisagarm2@gmail.com>
2024-12-19 23:34:02 +0545

    configured project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jarvis/homepage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text("hello world");
+    return const MaterialApp(
+      title: 'Jarvis',
+      home: Homepage(),
+    );
   }
 }
